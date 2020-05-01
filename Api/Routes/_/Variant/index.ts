3e56a9444094680e1b24ad9ce83bb562388155f8
Vince <vi.vinh0312@gmail.com>
2020-05-02 00:11:02 +0700

    modified

--- Api/Routes/_/Variant/index.ts
+++ Api/Routes/_/Variant/index.ts
@@ -2,14 +2,26 @@ import express from "express";
 const router = express.Router();
 import Config from "../../../../Config/Config";
 const { createConnection } = Config;
-import middleware from "../../../Middleware/AuthMiddleware";
-import jwtHelper from "../../../Helper/jwt.helper";
-const { isAuth, isAuthAsAdmin, isValidId_or_isAdmin } = middleware;
-const { verifyToken } = jwtHelper;
-const { ACCESS_TOKEN_SECRET } = process.env;
 
 import Join from "./Join/index";
 
 router.use("/", Join);
+
+router.get("/single/:id", (req, res) => {
+  createConnection.query(`select * from variant where variant_id = ${req.params.id}`, (err, result, field) => {
+    if ( err ) {
+      console.log(err);
+      res.json({
+        status: 404,
+        message: "Error while query to database"
+      });
+    } else {
+      res.json({
+        status: 200,
+        result
+      });
+    }
+  });
+});
 
 export default router;
